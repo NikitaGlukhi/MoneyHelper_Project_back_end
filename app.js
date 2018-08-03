1189a3855883b10701b0cd11d704f2eab7b4ec38
NikitaGlukhi <nikita.glukhi@mail.ru>
2018-08-03 17:12:50 +0300

    added basic functional with example storage file

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var initialData = require('./routes/initial_data');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/initial-data', initialData);
+
+var frontDir = path.join(__dirname, '../', 'front-end/dist');
+global.frontDir = frontDir;
+app.use(express.static(frontDir))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(global.frontDir + '/index.html'));
+  console.log("Had set connected");
+});
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
